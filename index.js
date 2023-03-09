5fe0fcf3f1bb5fce046aeba8095f6d8e2417fe3a
Thomaz <rm86681@estudante.fieb.edu.br>
2023-03-08 21:03:24 -0300

    Mais mudanças

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const menuToggle = document.querySelector('.menuToggle');
 const menu = document.querySelector('.menu');
+const iconmenu = document.querySelector('div#Iconmenu')
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   menu.classList.toggle('show');
 
 });
+
+
+
+
