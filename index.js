1bbe6e494f701965ae3c182f51ff7f6b14da7dce
Thomaz <rm86681@estudante.fieb.edu.br>
2023-03-09 10:36:21 -0300

    Mandando

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const menuToggle = document.querySelector('.menuToggle');
+//const menuToggle = document.querySelector('.menuToggle');
 const menu = document.querySelector('.menu');
-const iconmenu = document.querySelector('div#Iconmenu')
+const iconmenu = document.querySelector("ul#Iconmenu")
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  menu.classList.toggle('show');
+iconmenu.addEventListener('mouseenter', () => {
+  menu.classList.add('show');
 
 });
+
+menu.addEventListener('mouseleave', () => {
+  menu.classList.remove('show');
+
+});
+menu.addEventListener('mouseleave', () => {
+  menu.classList.remove('show');
+
+});
+
 
 
 
