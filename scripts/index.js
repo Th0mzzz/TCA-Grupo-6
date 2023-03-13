de962025da0a10c89ff6c054cd2e1abdaf3f0d1f
Thomaz <rm86681@estudante.fieb.edu.br>
2023-03-12 22:02:49 -0300

    mais mudanças

--- scripts/index.js
+++ scripts/index.js
@@ -15,23 +15,38 @@ menu.addEventListener('mouseleave', () => {
   menu.classList.remove('show');
 
 });
-
+const plus = document.querySelector("a#plus")
 const categorias = document.querySelector('li#categorias')
 const submenu = document.querySelector('div.submenu')
 const subitens = document.querySelector('a.subitens')
 
 categorias.addEventListener('click', () => {
-  submenu.classList.add('aberto');
+  if(submenu === 'aberto'){
+  submenu.classList.remove('aberto');}
+    else{
+      submenu.classList.add('aberto')
+    }
   
 
 });
+
 submenu.addEventListener('mouseleave', () => {
+  submenu.classList.remove('aberto');
+});
+menu.addEventListener('mouseleave', () => {
   submenu.classList.remove('aberto');
 });
 
 
+//-------------------SLIDESHOW--------------------//
+const prevButton = document.querySelector('#prevbtn')
+const nextButton = document.querySelector('#nextbtn')
+const slider = document.querySelector('.slider')
 
+prevButton.addEventListener('click', () => {
+    if(slider.style.left = '1100px'){
+      slider.style.right = '-1100px';
 
+    }
 
-
-
+}); 
